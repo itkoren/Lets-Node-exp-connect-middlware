9bd8cd77bca9e2df349edafbdfc5141668d8f604
Itai Koren <itkoren@gmail.com>
2014-05-24 15:19:48 +0300

    initial commit
    Signed-off-by: Itai Koren <itkoren@gmail.com>

---
+++ routes.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var router = express.Router();
+
+function secure (req, res, next) {
+    console.log("is Logged in???");
+    if (!req.session.logged_in) {
+        console.log("NA!!!!");
+        return res.send(403);
+    }
+    console.log("YEP!!!!");
+    next();
+}
+
+router.get("/login/:token", function (req, res) {
+    console.log("token:" + req.params.token);
+    if ("toki" === req.params.token) {
+        console.log("Login Success");
+        req.session.logged_in = true;
+        res.end("Login Success");
+        return;
+    }
+
+    console.log("Login Error");
+    res.end("Login Error");
+});
+
+router.get("/finance", secure, function (req, res) {
+    // secure!
+    res.end("100 SHEKEL");
+});
+
+router.get("*", function (req, res) {
+    res.end("Hello Express");
+});
+
+module.exports = router;
